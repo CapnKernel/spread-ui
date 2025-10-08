1cc969fdc4ce5d19be47cdbbe546eaf878878c18
Mitch Davis <mjd@afork.com>
2025-10-08 14:15:55 +1100

    Add first template

--- pyproj/spreadui/views.py
+++ pyproj/spreadui/views.py
@@ -2,5 +2,5 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def sheet(request):
-    return HttpResponse('Hello world')
+    return render(request, "spreadui/sheet.html")
 
