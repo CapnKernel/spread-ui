1c8afd6b91075a82fb0feeb00ea62a35c8d3e962
Mitch Davis <mjd@afork.com>
2025-10-09 14:57:22 +1100

    Show people info from Person table

--- pyproj/spreadui/views.py
+++ pyproj/spreadui/views.py
@@ -2,8 +2,10 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 from login_required import login_not_required
+from .models import Person
 
 
 @login_not_required
 def sheet(request):
-    return render(request, "spreadui/sheet.html")
+    people = Person.objects.all()
+    return render(request, "spreadui/sheet.html", {'people': people})
