c225d41d4c3e5dc1aefe808c49721971dad9fbf2
Mitch Davis <mjd@afork.com>
2025-10-09 14:42:41 +1100

    Added person model

--- pyproj/spreadui/admin.py
+++ pyproj/spreadui/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Person
 
-# Register your models here.
+
+@admin.register(Person)
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'email', 'age', 'created_at']
+    list_filter = ['age', 'created_at']
+    search_fields = ['first_name', 'last_name', 'email']
--- pyproj/spreadui/models.py
+++ pyproj/spreadui/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Person(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField()
+    age = models.PositiveIntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name_plural = "People"
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
